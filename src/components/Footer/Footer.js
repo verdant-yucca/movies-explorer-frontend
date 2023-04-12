853ad0d6b1ec3e70e5f08f1abad3e219ec2ee848
Roman Davydenko <Deadshot323rd@yandex.ru>
2023-04-12 12:54:44 +0800

    добавил на главную ссылку на страницу фильмы

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,13 @@
 import './Footer.css';
+import {Link} from "react-router-dom";
 
 function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer className="footer">
-      <p className="footer__text">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+      <p className="footer__text">Учебный проект Яндекс.Практикум х
+        <Link className={'footer__link footer__link_grey'} to={"/movies"}> BeatFilm.</Link>
+      </p>
       <div className="footer__container">
         <p className="footer__copy">&copy; {year}</p>
         <div className="footer__links">
